7108b41f5fc5d9d03881fa0bf20b0e6ce933c0ef
Arnaud <hayon.arnaud@gmail.com>
2024-10-09 11:13:18 +0200

    fix: gitignore

--- news.php
+++ news.php
@@ -24,29 +24,7 @@
         </aside>
         <main>
             <?php
-            /*
-              // C'est ici que le travail PHP commence
-              // Votre mission si vous l'acceptez est de chercher dans la base
-              // de données la liste des 5 derniers messsages (posts) et
-              // de l'afficher
-              // Documentation : les exemples https://www.php.net/manual/fr/mysqli.query.php
-              // plus généralement : https://www.php.net/manual/fr/mysqli.query.php
-             */
 
-            // Etape 1: Ouvrir une connexion avec la base de donnée.
-            $mysqli = new mysqli("localhost", "root", "", "socialnetwork");
-            //verification
-            if ($mysqli->connect_errno) {
-                echo "<article>";
-                echo ("Échec de la connexion : " . $mysqli->connect_error);
-                echo ("<p>Indice: Vérifiez les parametres de <code>new mysqli(...</code></p>");
-                echo "</article>";
-                exit();
-            }
-
-            // Etape 2: Poser une question à la base de donnée et récupérer ses informations
-            // cette requete vous est donnée, elle est complexe mais correcte, 
-            // si vous ne la comprenez pas c'est normal, passez, on y reviendra
             $laQuestionEnSql = "
                     SELECT posts.content,
                     posts.created,
@@ -62,44 +40,8 @@
                     ORDER BY posts.created DESC  
                     LIMIT 5
                     ";
-            $lesInformations = $mysqli->query($laQuestionEnSql);
-            // Vérification
-            if (!$lesInformations) {
-                echo "<article>";
-                echo ("Échec de la requete : " . $mysqli->error);
-                echo ("<p>Indice: Vérifiez la requete  SQL suivante dans phpmyadmin<code>$laQuestionEnSql</code></p>");
-                exit();
-            }
-
-            // Etape 3: Parcourir ces données et les ranger bien comme il faut dans du html
-            // NB: à chaque tour du while, la variable post ci dessous reçois les informations du post suivant.
-            while ($post = $lesInformations->fetch_assoc()) {
-                //la ligne ci-dessous doit etre supprimée mais regardez ce 
-                //qu'elle affiche avant pour comprendre comment sont organisées les information dans votre 
-                // echo "<pre>" . print_r($post, 1) . "</pre>";
-            
-                // @todo : Votre mission c'est de remplacer les AREMPLACER par les bonnes valeurs
-                // ci-dessous par les bonnes valeurs cachées dans la variable $post 
-                // on vous met le pied à l'étrier avec created
-                // 
-                // avec le ? > ci-dessous on sort du mode php et on écrit du html comme on veut... mais en restant dans la boucle
-                ?>
-                <article>
-                    <h3>
-                        <time><?php echo $post['created'] ?></time>
-                    </h3>
-                    <address><?php echo $post['author_name'] ?></address>
-                    <div>
-                        <p><?php echo $post['content'] ?></p>
-                    </div>
-                    <footer>
-                        <small>♥ <?php echo $post['like_number'] ?></small>
-                        <a href="">#<?php echo str_replace(',', ', #', $post['taglist']) ?></a>
-                    </footer>
-                </article>
-                <?php
-                // avec le <?php ci-dessus on retourne en mode php 
-            }// cette accolade ferme et termine la boucle while ouverte avant.
+            include 'utilitaires.php';
+            include 'article.php';
             ?>
 
         </main>
--- utilitaires.php
+++ utilitaires.php
@@ -3,18 +3,12 @@
 //Variable que la requete se fasse dans notre base de données avec condition d'erreur
 $lesInformations = $mysqli->query($laQuestionEnSql);
 // Vérification
-
-function verifInformations()
-{
-    global $lesInformations;
-    global $mysqli;
-    global $laQuestionEnSql;
-    if (!$lesInformations) {
-        echo ("<p>Échec de la requete :  . $mysqli->error </p>");
-        echo ("<p>Vérifiez la requete  SQL suivante dans phpmyadmin<code>$laQuestionEnSql</code></p>");
-        exit();
-    }
+if (!$lesInformations) {
+    echo ("<p>Échec de la requete :  . $mysqli->error </p>");
+    echo ("<p>Vérifiez la requete  SQL suivante dans phpmyadmin<code>$laQuestionEnSql</code></p>");
+    exit();
 }
+;
 
 
 
