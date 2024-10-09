6c4afb989bce5da16e7d24ffba9ad23d2f59f90e
HelenemLH <helenemlagarde@gmail.com>
2024-10-09 14:25:35 +0200

    update header.php dans followers et subscriptions

--- subscriptions.php
+++ subscriptions.php
@@ -1,33 +1,19 @@
 <!doctype html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8">
     <title>ReSoC - Mes abonnements</title>
     <meta name="author" content="Julien Falconnet">
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="style.css" />
 </head>
+
 <body>
-    <header>
-        <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-        <nav id="menu">
-            <a href="news.php">Actualités</a>
-            <a href="wall.php?user_id=5">Mur</a>
-            <a href="feed.php?user_id=5">Flux</a>
-            <a href="tags.php?tag_id=1">Mots-clés</a>
-        </nav>
-        <nav id="user">
-            <a href="#">Profil</a>
-            <ul>
-                <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-            </ul>
-        </nav>
-    </header>
-    
+    <?php include 'header.php'; ?>
+
     <div id="wrapper">
         <aside>
-            <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
             <section>
                 <h3>Présentation</h3>
                 <p>Sur cette page vous trouverez la liste des personnes dont l'utilisatrice
@@ -36,7 +22,7 @@
                 </p>
             </section>
         </aside>
-        
+
         <main class='contacts'>
             <?php
             include 'connect.php';
@@ -65,7 +51,7 @@
                 while ($abonnement = $result->fetch_assoc()) {
                     ?>
                     <article>
-                        <img src="user.jpg" alt="Portrait de l'utilisateur"/>
+                        <img src="user.jpg" alt="Portrait de l'utilisateur" />
                         <h3><?php echo $abonnement['alias']; ?></h3>
                         <p>ID: <?php echo $abonnement['id']; ?></p>
                     </article>
@@ -81,4 +67,5 @@
         </main>
     </div>
 </body>
+
 </html>
