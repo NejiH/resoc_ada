f637b3e2f22a6c4d53be504a9030cd1fbf5ffa7f
HelenemLH <helenemlagarde@gmail.com>
2024-10-10 11:49:04 +0200

    fix usurpedpost

--- usurpedpost.php
+++ usurpedpost.php
@@ -12,27 +12,8 @@ session_start();
 </head>
 
 <body>
-    <header>
-        <<<<<<< HEAD <img src="assets/resoc.jpg" alt="Logo de notre réseau social" />
-        =======
-        <img src="resoc.jpg" alt="Logo de notre réseau social" />
-        >>>>>>> 13d51cf (add: lien vers user profile)
-        <nav id="menu">
-            <a href="news.php">Actualités</a>
-            <a href="wall.php?user_id=5">Mur</a>
-            <a href="feed.php?user_id=5">Flux</a>
-            <a href="tags.php?tag_id=1">Mots-clés</a>
-        </nav>
-        <nav id="user">
-            <a href="#">Profil</a>
-            <ul>
-                <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-            </ul>
+    <?php include 'header.php'; ?>
 
-        </nav>
-    </header>
 
     <div id="wrapper">
 
@@ -45,13 +26,9 @@ session_start();
             <article>
                 <h2>Poster un message</h2>
                 <?php
-                /**
-                 * BD
-                 */
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-                /**
-                 * Récupération de la liste des auteurs
-                 */
+
+                include 'connect.php';
+
                 $listAuteurs = [];
                 $laQuestionEnSql = "SELECT * FROM users";
                 $lesInformations = $mysqli->query($laQuestionEnSql);
